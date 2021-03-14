2705a9098640503dfd9c34a5bb6d0146fd638fcd
Noah VanSickle <vansickn@union.edu>
2021-03-14 16:57:58 -0400

    added firestore backend, tirps, stations, and Trains

--- public/scripts/auth.js
+++ public/scripts/auth.js
@@ -1,3 +1,18 @@
+//get data
+db.collection('Trips').get().then(snapshot => {
+  setupTrips(snapshot.docs);
+});
+
+//listen for auth state changes
+auth.onAuthStateChanged(user => {
+  if (user){
+    console.log('user logged in', user);
+  } else{
+    console.log('user logged out');
+  }
+})
+
+
 //sign up
 
 const signupForm = document.querySelector('#signup-form');
@@ -11,36 +26,32 @@ signupForm.addEventListener('submit',(e) => {
   //signup user
 
   auth.createUserWithEmailAndPassword(email,password).then((cred) => {
-    console.log(cred);
       const modal = document.querySelector('#modal-signup');
       M.Modal.getInstance(modal).close();
       signupForm.reset();
     });
 
+});
+
+//logout
+const logout = document.querySelector('#logout');
+logout.addEventListener('click',(e) => {
+  e.preventDefault();
+  auth.signOut();
+});
+
+//login
+const loginForm = document.querySelector('#login-form');
+loginForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  //info
+  const email = loginForm['login-email'].value;
+  const password = loginForm['login-password'].value;
+
+  auth.signInWithEmailAndPassword(email,password).then((cred) => {
+    const modal = document.querySelector('#modal-login');
+    M.Modal.getInstance(modal).close();
+    loginForm.reset();
   });
-
-  //logout
-  const logout = document.querySelector('#logout');
-  logout.addEventListener('click',(e) => {
-    e.preventDefault();
-    auth.signOut().then(() => {
-      console.log('logged out');
-    })
-  });
-
-  //login
-  const loginForm = document.querySelector('#login-form');
-  loginForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-
-    //info
-    const email = loginForm['login-email'].value;
-    const password = loginForm['login-password'].value;
-
-    auth.signInWithEmailAndPassword(email,password).then((cred) => {
-      console.log(cred);
-      const modal = document.querySelector('#modal-login');
-      M.Modal.getInstance(modal).close();
-      loginForm.reset();
-    });
-  });
+});
--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,3 +1,24 @@
+const tripsList = document.querySelector('.trips');
+
+//setup trips
+const setupTrips = (data) => {
+  let html = '';
+  data.forEach(doc => {
+    const trip = doc.data();
+    const li = `
+    <li>
+      <div class="collapsible-header grey lighten-4">${trip.cost}</div>
+      <div class="collapsible-body white">${trip.Arrival}</div>
+    </li>
+    `;
+    html += li;
+
+  });
+
+  tripsList.innerHTML = html;
+
+}
+
 // setup materialize components
 document.addEventListener('DOMContentLoaded', function() {
 
