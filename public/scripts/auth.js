91f4fcaa134935ba6a093691ef6ab42c9d780098
Noah VanSickle <vansickn@union.edu>
2021-03-14 16:05:04 -0400

    added sign in, sign out, login

---
+++ public/scripts/auth.js
@@ -0,0 +1,46 @@
+//sign up
+
+const signupForm = document.querySelector('#signup-form');
+signupForm.addEventListener('submit',(e) => {
+  e.preventDefault(); //doesn't refresh the page
+
+  //user info
+  const email = signupForm['signup-email'].value;
+  const password = signupForm['signup-password'].value;
+
+  //signup user
+
+  auth.createUserWithEmailAndPassword(email,password).then((cred) => {
+    console.log(cred);
+      const modal = document.querySelector('#modal-signup');
+      M.Modal.getInstance(modal).close();
+      signupForm.reset();
+    });
+
+  });
+
+  //logout
+  const logout = document.querySelector('#logout');
+  logout.addEventListener('click',(e) => {
+    e.preventDefault();
+    auth.signOut().then(() => {
+      console.log('logged out');
+    })
+  });
+
+  //login
+  const loginForm = document.querySelector('#login-form');
+  loginForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    //info
+    const email = loginForm['login-email'].value;
+    const password = loginForm['login-password'].value;
+
+    auth.signInWithEmailAndPassword(email,password).then((cred) => {
+      console.log(cred);
+      const modal = document.querySelector('#modal-login');
+      M.Modal.getInstance(modal).close();
+      loginForm.reset();
+    });
+  });
